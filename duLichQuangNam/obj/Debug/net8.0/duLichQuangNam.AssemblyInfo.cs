dae3c02fdbcd77326e041076f4a2652ab3577345
phongdang1 <phongdbde160574@fpt.edu.vn>
2025-06-08 10:36:16 +0700

    2

--- duLichQuangNam/obj/Debug/net8.0/duLichQuangNam.AssemblyInfo.cs
+++ duLichQuangNam/obj/Debug/net8.0/duLichQuangNam.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("e47c65dc-f806-4f94-ab0b-bfb1b17db249")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdc2db395ab4e6dcb5858f97b284c6a64bbdf84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90495406d4c46384b16e2a5b535209b9922ad14")]
 [assembly: System.Reflection.AssemblyProductAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
