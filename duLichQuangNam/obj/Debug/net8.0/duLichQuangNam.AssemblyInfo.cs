2cdc2db395ab4e6dcb5858f97b284c6a64bbdf84
phongdang1 <phongdbde160574@fpt.edu.vn>
2025-06-08 10:01:56 +0700

    1

--- duLichQuangNam/obj/Debug/net8.0/duLichQuangNam.AssemblyInfo.cs
+++ duLichQuangNam/obj/Debug/net8.0/duLichQuangNam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ace168969aed836103c7f2bbe00a1a3fc5767a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a5bc08a385654a6f96382e3afe056bdb73188b")]
 [assembly: System.Reflection.AssemblyProductAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("duLichQuangNam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
